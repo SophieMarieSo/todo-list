ff9f328d968994cb957072fdde04d0b3379298e9
AhnEunJin <modic970515@gmail.com>
2024-10-20 05:05:28 +0900

    포트번호 환경변수로 설정

--- app.js
+++ app.js
@@ -17,4 +17,7 @@ mongoose
   .then(() => console.log('mongoose connected'))
   .catch((err) => console.log('DB connection fail', err));
 
-app.listen(5000, () => console.log('server is on 5000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
