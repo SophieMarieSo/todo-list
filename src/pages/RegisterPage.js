3c9098cda0877c7b12ad2d716dd5f538186e327d
AhnEunJin <modic970515@gmail.com>
2024-10-21 22:23:56 +0900

    회원가입 fe 구현

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,33 +1,87 @@
-import React from "react";
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
+import React, { useState } from 'react';
+import { Alert } from 'react-bootstrap';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import api from '../utils/api';
+import { useNavigate } from 'react-router-dom';
+
+const initialForm = {
+  name: '',
+  email: '',
+  password: '',
+  reEnterPassword: '',
+};
 
 const RegisterPage = () => {
+  const [form, setForm] = useState(initialForm);
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      // 첫번째 비밀번호와 두번째 비밀번호가 일치하는지 확인
+      if (form.password !== form.reEnterPassword) {
+        throw new Error('비밀번호가 일치하지 않습니다. 다시 입력해주세요');
+      }
+
+      const data = {
+        name: form.name,
+        password: form.password,
+        email: form.email,
+      };
+      const resp = await api.post('/user', data);
+      if (resp.status === 200) {
+        navigate('/login');
+      } else {
+        throw new Error(resp.data.Error);
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
   return (
-    <div className="display-center">
-      <Form className="login-box">
+    <div className='display-center'>
+      {error && <Alert variant='danger'>{error}</Alert>}
+      <Form className='login-box' onSubmit={handleSubmit}>
         <h1>회원가입</h1>
-        <Form.Group className="mb-3" controlId="formName">
+        <Form.Group className='mb-3' controlId='formName'>
           <Form.Label>Name</Form.Label>
-          <Form.Control type="string" placeholder="Name" />
+          <Form.Control
+            type='string'
+            onChange={(e) => setForm({ ...form, name: e.target.value })}
+          />
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Group className='mb-3' controlId='formBasicEmail'>
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            type='email'
+            onChange={(e) => setForm({ ...form, email: e.target.value })}
+          />
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Group className='mb-3' controlId='formBasicPassword'>
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            type='password'
+            onChange={(e) => setForm({ ...form, password: e.target.value })}
+          />
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Group className='mb-3' controlId='formBasicPassword'>
           <Form.Label>re-enter the password</Form.Label>
-          <Form.Control type="password" placeholder="re-enter the password" />
+          <Form.Control
+            type='password'
+            onChange={(e) =>
+              setForm({ ...form, reEnterPassword: e.target.value })
+            }
+          />
         </Form.Group>
 
-        <Button className="button-primary" type="submit">
+        <Button className='button-primary' type='submit'>
           회원가입
         </Button>
       </Form>
