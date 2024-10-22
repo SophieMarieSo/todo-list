d0cebc8c06e884b511b9646f549339ecea261aaf
AhnEunJin <modic970515@gmail.com>
2024-10-22 23:20:21 +0900

    토큰으로 사용자 정보 찾기

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,35 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./App.css";
-import { Routes, Route } from "react-router-dom";
-import LoginPage from "./pages/LoginPage";
-import TodoPage from "./pages/TodoPage";
-import RegisterPage from "./pages/RegisterPage";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import TodoPage from './pages/TodoPage';
+import RegisterPage from './pages/RegisterPage';
+import { useState } from 'react';
+import PrivateRoute from './route/PrivateRoute';
 
 function App() {
+  const [user, setUser] = useState();
+
+  const getUser = async () => {
+    try {
+      const token = sessionStorage.getItem('token');
+      // const resp = api
+    } catch (error) {}
+  };
+
   return (
     <Routes>
-      <Route path="/" element={<TodoPage />} />
-      <Route path="/register" element={<RegisterPage />} />
+      <Route
+        path='/'
+        element={
+          <PrivateRoute user={user}>
+            <TodoPage />
+          </PrivateRoute>
+        }
+      />
+      <Route path='/register' element={<RegisterPage />} />
 
-      <Route path="/login" element={<LoginPage />} />
+      <Route path='/login' element={<LoginPage />} />
     </Routes>
   );
 }
