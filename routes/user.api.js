7cbb1834db3486104641778c584aecc0974dddb8
AhnEunJin <modic970515@gmail.com>
2024-10-22 23:19:55 +0900

    토큰으로 사용자 정보 찾기

--- routes/user.api.js
+++ routes/user.api.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const { authenticate } = require('../controller/auth.controller');
 const userController = require('../controller/user.controller');
-const { createUser, loginWithEmail } = userController;
+const { createUser, loginWithEmail, getUser } = userController;
 
 // 회원가입
 router.post('/', createUser);
 
 // 로그인
 router.post('/login', loginWithEmail);
+
+// 토큰을 통해 사용자 id 추출 -> 해당 id로 사용자 객체 보내기
+router.get('/me', authenticate, getUser);
 
 module.exports = router;
