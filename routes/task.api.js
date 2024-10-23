7635f810283f8883c8c900e30f8c027093870974
AhnEunJin <modic970515@gmail.com>
2024-10-23 22:44:20 +0900

    login redirect & author 추가

--- routes/task.api.js
+++ routes/task.api.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const taskController = require('../controller/task.controller');
+const authController = require('../controller/auth.controller');
 const router = express.Router();
 const { createTask, getTask, updateTask, deleteTask } = taskController;
 
-router.post('/', createTask);
+router.post('/', authController.authenticate, createTask);
 router.get('/', getTask);
 router.put('/:id', updateTask);
 router.delete('/:id', deleteTask);
